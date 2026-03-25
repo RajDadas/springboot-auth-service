6139f509300debd017b591254a6c48d9d9eee614
Rajdatta Kashinath dadas <101989702+RajDadas@users.noreply.github.com>
2026-03-25 22:05:36 +0530

    Project files added

---
+++ springboot-auth-service/src/main/java/com/example/auth/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.auth.config;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+http.csrf(csrf->csrf.disable())
+.authorizeHttpRequests(auth->auth
+.requestMatchers("/auth/**","/v3/api-docs/**","/swagger-ui/**").permitAll()
+.anyRequest().authenticated()
+);
+return http.build();
+}
+
+@Bean
+public PasswordEncoder passwordEncoder(){
+return new BCryptPasswordEncoder();
+}
+}
